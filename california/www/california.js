e6dec0094ee63a9a361c4a2338877843c0d76862
baltasarq <baltasarq@gmail.com>
2018-05-16 07:58:33 +0200

    Finished transition to fi.js

--- california/www/california.js
+++ california/www/california.js
@@ -23,7 +23,8 @@ ctrl.setVersion( "1.0 20180513" );
 var locRoad = ctrl.places.creaLoc(
     "Oscura y desierta carretera",
     [ "carretera" ],
-    "Notas el aire en tu pelo mientras escuchas ${la radio del coche, ex radio} \
+    "Notas el aire en tu pelo mientras escuchas \
+    ${la radio del coche, ex radio} \
     conduciendo por una carretera serpenteante. \
     Cansado, ${debes parar por esta noche, n}, y ves en la distancia \
     el ${cartel de un motel, ex cartel}, cimbreante."
@@ -32,7 +33,8 @@ var locRoad = ctrl.places.creaLoc(
 locRoad.pic = "res/road.jpg";
 
 locRoad.getExitsDesc = function() {
-    return "Tu cabeza te pesa y tu mirada se estrecha: debes ${parar, n} por esta noche.";
+    return "Tu cabeza te pesa y tu mirada se estrecha: \
+            debes ${parar, n} por esta noche.";
 };
 
 
@@ -47,16 +49,16 @@ const objRadio = ctrl.creaObj(
 objRadio.songShown = false;
 objRadio.preExamine = function() {
     var toret = this.desc;
-    
+
     if ( !this.songShown ) {
-        toret += "<br/><i>On a dark desert highway, cool wind in my hair. \
-                    Warm smell of colitas, rising up through the air. \
-                    Up ahead in the distance, I saw a shimmering light. \
-                    My head grew heavy and my sight grew dim, \
-                    I had to stop for the night.</i>";
+        toret += "<br/><i>\"On a dark desert highway, cool wind in my hair.\
+                    <br/>Warm smell of colitas, rising up through the air.\
+                    <br/>Up ahead in the distance, I saw a shimmering light. \
+                    <br/>My head grew heavy and my sight grew dim, \
+                    <br/>I had to stop for the night.\"</i>";
         this.songShown = true;
     }
-    
+
     return toret;
 };
 
@@ -64,7 +66,8 @@ objRadio.preExamine = function() {
 const objCartelMotel = ctrl.creaObj(
     "cartel",
     [ "anuncio" ],
-    "Un motel con un cartel que rezuma elegancia destaca ahora mismo en la distancia.",
+    "Un motel con un cartel que rezuma elegancia \
+     destaca ahora mismo en la distancia.",
     locRoad,
     Ent.Scenery
 );
@@ -73,9 +76,204 @@ objCartelMotel.postExamine = function() {
     ctrl.places.showPic( "res/motel.jpg" );
 }
 
-// --------------------------------------------------------------- Road --
+// --------------------------------------------------------------- PL3 --------
+const locPL1 = ctrl.places.creaLoc(
+    "Aparcamiento 1",
+     [ "aparcamiento", "parking" ],
+     "El aparcamiento se extiende en ambas direcciones, \
+      desde la recepción al extremo en septentrión. \
+      Enfrente puedes ver ${una puerta, ex hab11}."
+);
+
+locPL1.getExitsDesc = function() {
+    return "A tu derecha, la ${recepción, e}, \
+            a tu izquierda otro ${aparcamiento, o}.";
+};
+
+const objPuertaHab11 = ctrl.creaObj(
+    "puerta 11",
+    [ "puerta" ],
+    "puerta 11",
+    locPL1,
+    Ent.Scenery
+);
+
+// --------------------------------------------------------------- PL2 --------
+const locPL2 = ctrl.places.creaLoc(
+    "Aparcamiento 2",
+    [ "aparcamiento", "parking" ],
+    "El aparcamiento se extiende en ambas direcciones, \
+     desde la recepción al extremo en septentrión. \
+     Enfrente puedes ver ${una puerta, ex hab11}."
+);
+
+locPL2.setExitBi( "e", locPL1 );
+locPL2.getExitsDesc = function() {
+    return "A tu derecha, un ${aparcamiento, e}, \
+            a tu izquierda ${otro, o}, cierto.";
+};
+
+const objPuertaHab12 = ctrl.creaObj(
+    "puerta 12",
+    [ "puerta" ],
+    "puerta 12",
+    locPL2,
+    Ent.Scenery
+);
+
+// --------------------------------------------------------------- PL3 --------
+const locPL3 = ctrl.places.creaLoc(
+    "Aparcamiento 3",
+    [ "aparcamiento", "parking" ],
+    "El aparcamiento se extiende en ambas direcciones, \
+     desde la recepción al extremo en septentrión. \
+     Enfrente puedes ver ${una puerta, ex hab13}."
+);
+
+locPL3.setExitBi( "e", locPL2 );
+locPL3.getExitsDesc = function() {
+    return "A tu derecha, un ${aparcamiento, e}, \
+            a tu izquierda ${otro, o}, cierto.";
+};
+
+const objPuertaHab13 = ctrl.creaObj(
+    "puerta 13",
+    [ "puerta" ],
+    "puerta 13",
+    locPL3,
+    Ent.Scenery
+);
+
+// --------------------------------------------------------------- PL4 --------
+const locPL4 = ctrl.places.creaLoc(
+    "Aparcamiento 4",
+    [ "aparcamiento", "parking" ],
+    "El aparcamiento se extiende en ambas direcciones, \
+     desde la recepción al extremo en septentrión. \
+     Enfrente puedes ver ${una puerta, ex hab14}."
+);
+
+locPL4.setExitBi( "e", locPL3 );
+locPL4.getExitsDesc = function() {
+    return "A tu derecha, un ${aparcamiento, e}, \
+            a tu izquierda ${otro, o}, cierto.";
+};
+
+const objPuertaHab14 = ctrl.creaObj(
+    "puerta 14",
+    [ "puerta" ],
+    "puerta 14",
+    locPL4,
+    Ent.Scenery
+);
 
 
+// ------------------------------------------------------ South corner --------
+const locSouthCorner = ctrl.places.creaLoc(
+    "Esquina",
+    [ "aparcamiento", "parking" ],
+    "Termina aquí la fachada del edificio, junto a una \
+     ${máquina de hielo, ex maquina}."
+);
+
+locSouthCorner.setExitBi( "e", locPL4 );
+locSouthCorner.getExitsDesc = function() {
+    return "A tu derecha, un ${aparcamiento, e}, \
+            hacia delante, por la ${esquina, n}, al motel das un rodeo.";
+};
+
+const objIceMachine = ctrl.creaObj(
+    "máquina de hielo",
+    [ "maquina", "hielo" ],
+    "La máquina de hielo tiene algo de hielo en su interior. Poco a poco \
+     se irá formando más.",
+    locSouthCorner
+);
+
+// ------------------------------------------------------ Alley ---------------
+const locAlley = ctrl.places.creaLoc(
+    "Callejón",
+    [ "callejon" ],
+    "El callejón termina en un muro donde se alinean \
+    ${cubos de basura, ex cubos}."
+);
+
+locAlley.setExitBi( "s", locSouthCorner );
+locAlley.getExitsDesc = function() {
+    return "A tu derecha, un ${aparcamiento, e}, \
+                hacia delante, por la ${esquina, n}, al motel das un rodeo.";
+};
+
+const objCubosBasura = ctrl.creaObj(
+    "cubos de basura",
+    [ "cubo", "cubos", "basura" ],
+    "Cerrados pero apestosos, no resultan nada golosos.",
+    locAlley
+);
+
+// ------------------------------------------------------ Reception -----------
+const locReception = ctrl.places.creaLoc(
+    "Recepción",
+    [ "recepcion" ],
+    "La ${mujer, ex mujer} que lleva el hotel, alerta, \
+     espera desde el dintel de la puerta. \
+     Un ${aparato de radio, ex radio} reproducía una agradable melodía."
+);
+
+locReception.setExitBi( "s", locRoad );
+locReception.getExitsDesc = function() {
+    return "La recepción es tu casa, aunque puedes ${salir ahora, s}.";
+};
+
+const objRadioRecepcion = ctrl.creaObj(
+    "radio",
+    [ "aparato" ],
+    "",
+    locReception
+);
+
+objRadioRecepcion.listened = false;
+objRadioRecepcion.preExamine = function() {
+    var toret = this.desc;
+
+    if ( !objRadioRecepcion.listened ) {
+        toret += "<br/>\"<i>There she stood in the doorway;\
+                   <br/>I heard the mission bell.\
+                   <br/>And I was thinking to myself:\
+                   <br/>'This could be heaven or this could be Hell.'\"</i>";
+    } else {
+        toret += "Reproducía <i>pop</i> como compañía";
+    }
+
+    return toret;
+};
+
+const pnjWoman = ctrl.personas.creaPersona(
+    "mujer",
+    [ "recepcionista" ],
+    "Su belleza hostiga tu mente, es ciertamente una mujer hermosa.\
+     Confuso por su presencia, apenas puedes pensar en otra cosa.",
+    locReception
+);
+
+// ------------------------------------------------------ Hall ---------------
+const locHall = ctrl.places.creaLoc(
+    "Hall",
+    [ "hall" ],
+    ""
+);
+
+locHall.setExitBi( "s", locReception );
+locHall.getExitsDesc = function() {
+    return "La recepción es tu casa, aunque puedes ${salir ahora, s}.";
+};
+
+const objRadioHall = ctrl.creaObj(
+    "radio",
+    [ "aparato" ],
+    "",
+    locHall
+);
 
 // Achievements ================================================================
 ctrl.achievements.add( "flee_note",
